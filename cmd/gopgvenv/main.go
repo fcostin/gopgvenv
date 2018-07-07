93c204728f6fd47ae362bbfb7790a72d7d587aa3
rfc <reuben.fletchercostin@gmail.com>
2018-07-07 16:29:46 +1000

    discover postgres bindir

--- cmd/gopgvenv/main.go
+++ cmd/gopgvenv/main.go
@@ -1,7 +1,19 @@
 package main
 
 import "fmt"
+import "os/exec"
+import "strings"
+
+func getPGBinDir() string {
+	cmd := exec.Command("pg_config", "--bindir")
+	out, err := cmd.Output()
+	if err != nil {
+		panic(err)
+	}
+	return strings.TrimSpace(string(out))
+}
 
 func main() {
-	fmt.Println("hey there")
+	pgBinDir := getPGBinDir()
+	fmt.Println("pg_bindir=", pgBinDir)
 }
