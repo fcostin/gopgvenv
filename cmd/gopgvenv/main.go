515d9385b3267998ff12f362974cfe6336db3c4b
rfc <reuben.fletchercostin@gmail.com>
2018-07-07 17:24:46 +1000

    initdb in workspace

--- cmd/gopgvenv/main.go
+++ cmd/gopgvenv/main.go
@@ -2,31 +2,56 @@ package main
 
 import "fmt"
 import "io/ioutil"
+import "log"
 import "os"
 import "os/exec"
+import "path/filepath"
 import "strings"
 
 func getPGBinDir() string {
 	cmd := exec.Command("pg_config", "--bindir")
-	out, err := cmd.Output()
+	outb, err := cmd.CombinedOutput()
 	if err != nil {
 		panic(err)
 	}
-	return strings.TrimSpace(string(out))
+	return strings.TrimSpace(string(outb))
+}
+
+func pgInitDb(pgBinDir string, pgDataDir string) {
+	binPath := filepath.Join(pgBinDir, "pg_ctl")
+	args := []string{"initdb", "--pgdata", pgDataDir}
+	cmd := exec.Command(binPath, args...)
+	outb, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Panicln("exec failed:", binPath, args, string(outb))
+	}
 }
 
 func main() {
 	pgBinDir := getPGBinDir()
+
 	fmt.Println("pg_bindir=", pgBinDir)
-	workspace, err := ioutil.TempDir("", "gopgvenv_")
+
+	workDir, err := ioutil.TempDir("", "gopgvenv_")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("workspace=", workspace)
 	defer func() {
-		err = os.RemoveAll(workspace)
+		err = os.RemoveAll(workDir)
 		if err != nil {
 			panic(err)
 		}
 	}()
+
+	fmt.Println("workDir=", workDir)
+
+	pgDataDir := filepath.Join(workDir, "pgdata")
+	err = os.MkdirAll(pgDataDir, 0755)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("pgDataDir=", pgDataDir)
+
+	pgInitDb(pgBinDir, pgDataDir)
 }
